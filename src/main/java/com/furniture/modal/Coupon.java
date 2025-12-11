7d6d3ceeb007dcf2bf5ed3ad7dd91e7553baa05b
ThanhNgo1007 <nhthanh1007@gmail.com>
2025-12-11 09:54:20 +0700

    fix: Add @JsonIgnore to Coupon.usedByUser to fix 500 error

--- src/main/java/com/furniture/modal/Coupon.java
+++ src/main/java/com/furniture/modal/Coupon.java
@@ -1,12 +1,22 @@
 package com.furniture.modal;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -33,5 +43,7 @@ public class Coupon {
     private boolean isActive=true;
 
     @ManyToMany(mappedBy = "usedCoupons")
+    @JsonIgnore
     private Set<User> usedByUser = new HashSet<>();
 }
+
